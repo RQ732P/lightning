f5a1aefa770d3ed3fb1c18af8709e920052bd820
RQ732P <RQ732P@gmail.com>
2018-07-13 12:00:45 -0400

    message

--- common/initial_commit_tx.c
+++ common/initial_commit_tx.c
@@ -194,11 +194,17 @@ struct bitcoin_tx *initial_commit_tx(const tal_t *ctx,
 
 
 		printf("initial_commit_tx: add to remote\n");
-		unsigned char *sw = (unsigned char *) &(keyset->other_payment_key);
-		printf("other payment key\n");
+		
+
+		unsigned char der_keys[PUBKEY_DER_LEN];
+		pubkey_to_der(der_keys, sw);
+		unsigned char *sw = der_keys;
+
+		printf("other payment key is\n");
 		for (int i = 0; i < 64; ++i)
 			printf("%02x", (unsigned int) *sw++);
 		printf("\n");
+
 
 
 
